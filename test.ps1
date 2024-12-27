c3f76e460c5fef5e7690fca82aab8dc5bbd7ee15
hlefranc01 <hubert.lefranc@gmail.com>
2024-12-27 17:50:06 +0100

    delete

--- test.ps1
+++
@@ -1,7 +0,0 @@
-$ShareListToCreate = Import-Csv -Delimiter "`t" -Path $ShareCSVFile
-
- foreach ($ShareListToCreateItem in $ShareListToCreate){
-    $strShare = $ShareListToCreateItem.NewShareName
-    $strServer = $ShareListToCreateItem.NewFileServerName
-    $strPath = $ShareListToCreateItem.NewSharePath
-    
